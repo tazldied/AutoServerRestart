7c892a9f39cac8ccb4304707756928fd5e4faf40
tazl <82299965+tazldied@users.noreply.github.com>
2026-03-22 21:21:16 -0400

    fixed jank

--- src/main/java/cc/tazl/serverrestart/restart.java
+++ src/main/java/cc/tazl/serverrestart/restart.java
@@ -3,99 +3,106 @@ package cc.tazl.serverrestart;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class restart extends JavaPlugin {
-    private static final long RESTART_INTERVAL = 5 * 60 * 60 * 20L; // 5 hours in ticks (20 ticks = 1 second)
-    private static final long FIVE_MINUTES = 5 * 60 * 20L;
-    private static final long ONE_MINUTE = 60 * 20L;
-    private static final long TEN_SECONDS = 10 * 20L;
 
-    private long ticksUntilNextRestart;
+    private static final long RESTART_INTERVAL_TICKS = 5 * 60 * 60 * 20L; // 5 hours
+    private static final long FIVE_MINUTES_TICKS = 5 * 60 * 20L;
+    private static final long ONE_MINUTE_TICKS = 60 * 20L;
+    private static final long TEN_SECONDS_TICKS = 10 * 20L;
+
+    private long nextRestartAtMillis;
 
     @Override
     public void onEnable() {
-        scheduleRestart();
-        getCommand("restarttime").setExecutor(new RestartTimeCommandExecutor());
+        saveDefaultConfig();
+
+        if (getCommand("restarttime") != null) {
+            getCommand("restarttime").setExecutor(this);
+        } else {
+            getLogger().warning("Command 'restarttime' not found in plugin.yml");
+        }
+
+        scheduleRestartCycle();
+        getLogger().info("Auto server restart enabled.");
     }
 
-    private void scheduleRestart() {
-        ticksUntilNextRestart = RESTART_INTERVAL - FIVE_MINUTES;
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                alertPlayers();
-                ticksUntilNextRestart = RESTART_INTERVAL - FIVE_MINUTES;
-            }
-        }.runTaskTimer(this, ticksUntilNextRestart, RESTART_INTERVAL);
-
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                if (ticksUntilNextRestart > 0) {
-                    ticksUntilNextRestart--;
-                }
-            }
-        }.runTaskTimer(this, 0L, 1L);
+    @Override
+    public void onDisable() {
+        getLogger().info("Auto server restart disabled.");
     }
 
-    private void alertPlayers() {
-        // Alert 5 minutes before restart
+    private void scheduleRestartCycle() {
+        long restartIntervalMillis = RESTART_INTERVAL_TICKS * 50L;
+        nextRestartAtMillis = System.currentTimeMillis() + restartIntervalMillis;
+
         new BukkitRunnable() {
             @Override
             public void run() {
-                Bukkit.broadcastMessage(ChatColor.RED + "Server will restart in 5 minutes!");
+                startCountdown();
+                nextRestartAtMillis = System.currentTimeMillis() + restartIntervalMillis;
             }
-        }.runTaskLater(this, 0L);
+        }.runTaskTimer(this, RESTART_INTERVAL_TICKS - FIVE_MINUTES_TICKS, RESTART_INTERVAL_TICKS);
+    }
 
-        // Alert 1 minute before restart
+    private void startCountdown() {
+        Bukkit.broadcastMessage(ChatColor.RED + "Server will restart in 5 minutes!");
+
         new BukkitRunnable() {
             @Override
             public void run() {
                 Bukkit.broadcastMessage(ChatColor.RED + "Server will restart in 1 minute!");
             }
-        }.runTaskLater(this, FIVE_MINUTES - ONE_MINUTE);
+        }.runTaskLater(this, FIVE_MINUTES_TICKS - ONE_MINUTE_TICKS);
 
-        // Alert 10 seconds before restart
         new BukkitRunnable() {
             @Override
             public void run() {
                 Bukkit.broadcastMessage(ChatColor.RED + "Server will restart in 10 seconds!");
             }
-        }.runTaskLater(this, FIVE_MINUTES - TEN_SECONDS);
+        }.runTaskLater(this, FIVE_MINUTES_TICKS - TEN_SECONDS_TICKS);
 
-        // Restart the server
+        for (int i = 10; i >= 1; i--) {
+            final int seconds = i;
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    Bukkit.broadcastMessage(ChatColor.RED + "Restarting in " + seconds + "...");
+                }
+            }.runTaskLater(this, FIVE_MINUTES_TICKS - (seconds * 20L));
+        }
+
         new BukkitRunnable() {
             @Override
             public void run() {
                 Bukkit.broadcastMessage(ChatColor.RED + "Server is restarting now!");
                 Bukkit.shutdown();
             }
-        }.runTaskLater(this, FIVE_MINUTES);
+        }.runTaskLater(this, FIVE_MINUTES_TICKS);
     }
 
-
-    public class RestartTimeCommandExecutor implements CommandExecutor {
-        @Override
-        public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-            if (command.getName().equalsIgnoreCase("restarttime")) {
-                long secondsUntilRestart = ticksUntilNextRestart / 20;
-                long minutesUntilRestart = secondsUntilRestart / 60;
-                long hoursUntilRestart = minutesUntilRestart / 60;
-
-                secondsUntilRestart %= 60;
-                minutesUntilRestart %= 60;
-
-                String timeMessage = String.format("Time until next restart: %d hours, %d minutes, %d seconds",
-                        hoursUntilRestart, minutesUntilRestart, secondsUntilRestart);
-
-                sender.sendMessage(ChatColor.GREEN + timeMessage);
-                return true;
-            }
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!command.getName().equalsIgnoreCase("restarttime")) {
             return false;
         }
+
+        long millisLeft = nextRestartAtMillis - System.currentTimeMillis();
+        if (millisLeft < 0) millisLeft = 0;
+
+        long totalSeconds = millisLeft / 1000L;
+        long hours = totalSeconds / 3600L;
+        long minutes = (totalSeconds % 3600L) / 60L;
+        long seconds = totalSeconds % 60L;
+
+        sender.sendMessage(ChatColor.GREEN + String.format(
+                "Time until next restart: %d hours, %d minutes, %d seconds",
+                hours, minutes, seconds
+        ));
+
+        return true;
     }
 }
